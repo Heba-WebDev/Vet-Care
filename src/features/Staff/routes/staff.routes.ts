eaa9db030c3b6aec0acd26668d0d22c3db5b3cd7
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-27 01:06:03 +0300

    Added all routes for staff

---
+++ src/features/Staff/routes/staff.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+// Token verification
+import { verifyToken } from "../../../middlewares/verifyToken";
+// Controllers
+import { staffRegistration } from "../controllers/registration.staff";
+import { staffDelete } from "../controllers/delete.staff";
+import { staffVerify } from "../controllers/verify.staff";
+import { staffUpdate } from "../controllers/update.staff";
+import { staffLogin } from "../controllers/login.staff";
+// Input validation schemas
+import { staffRegistrationValidation } from "../input-validation/registration.staff.schema";
+import { staffLoginValidation } from "../input-validation/login.staff.schema";
+import { staffDeleteValidation } from "../input-validation/delete.staff.schema";
+import { staffVerifyValidation } from "../input-validation/verify.staff.schema";
+import { staffUpdateValidation } from "../input-validation/update.staff.schema";
+
+
+
+const staffRouter = Router();
+
+staffRouter.route("/register").post(staffRegistrationValidation, staffRegistration);
+
+staffRouter.route("/login").post(staffLoginValidation, staffLogin);
+
+staffRouter.route("/").delete(verifyToken, staffDeleteValidation, staffDelete);
+
+staffRouter.route("/verify").put(verifyToken, staffVerifyValidation, staffVerify);
+
+staffRouter.route("/").patch(verifyToken, staffUpdateValidation, staffUpdate);
+
+export { staffRouter}
