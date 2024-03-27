dcae42e8cb3bf2ec7ac83be1bfa37d4ae0226bef
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-28 00:55:15 +0300

    Created the controller for vets get-former-vets route

---
+++ src/features/Vets/controllers/get-all-former.vets.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { statusCode } from "../../../utils/httpStatusCode";
+import { prisma } from "../../../config/prisma";
+import { wrapper } from "../../../middlewares/asyncWrapper";
+import { globalError } from "../../../utils/globalError";
+const { SUCCESS, FAIL } = statusCode;
+
+const getAllFormerVets = wrapper(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.decodedToken;
+    if(token?.permission_type !== "Admin") {
+        const err = new globalError("Unauthorized to perform this action.", 401
+        ,FAIL)
+        return next(err);
+    }
+    const page = parseInt(req.query.page as string) || 1; // Default page 1
+    const limit = parseInt(req.query.limit as string) || 15; // Default 15 results per page
+    const offset = (page - 1) * limit;
+    await prisma.formerVets.findMany({
+        skip: offset,
+        take: limit,
+    }).then((result) => {
+        return res.status(200).send({
+        status: SUCCESS,
+        message: null,
+        data: result
+    });
+    });
+});
+
+export { getAllFormerVets }
