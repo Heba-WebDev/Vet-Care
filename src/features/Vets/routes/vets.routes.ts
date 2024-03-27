4e9c4b940bd28bf0d091a6960a5a5d33ebe500ac
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-28 01:01:15 +0300

    Added all routes for vets, including token verification and input validation

---
+++ src/features/Vets/routes/vets.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+// Token verification
+import { verifyToken } from "../../../middlewares/verifyToken";
+
+// Controllers
+import { getAllVets } from "../controllers/get-all.vets";
+import { getAllFormerVets } from "../controllers/get-all-former.vets";
+import { vetsRegistration } from "../controllers/registration.vets";
+import { vetsLogin } from "../controllers/login.vets";
+import { vetsUpdate } from "../controllers/update.vets";
+import { vetsDelete } from "../controllers/delete.vets";
+
+// Input validation schemas
+import { vetsRegistrationValidation } from "../input-validation/registration.vets.schema";
+import { vetsLoginValidation } from "../input-validation/login.vets.schema";
+import { vetsUpdateValidation } from "../input-validation/update.vets.schema";
+import { vetsDeleteValidation } from "../input-validation/delete.vets.schema";
+
+
+const vetsRouter = Router();
+
+vetsRouter.route("/current").get(verifyToken, getAllVets);
+
+vetsRouter.route("/former").get(verifyToken, getAllFormerVets);
+
+vetsRouter.route("/register").post(vetsRegistrationValidation, vetsRegistration);
+
+vetsRouter.route("/login").post(verifyToken, vetsLoginValidation, vetsLogin);
+
+vetsRouter.route("/").put(verifyToken, vetsUpdateValidation, vetsUpdate);
+
+vetsRouter.route("/").delete(verifyToken, vetsDeleteValidation, vetsDelete);
+
+export { vetsRouter }
