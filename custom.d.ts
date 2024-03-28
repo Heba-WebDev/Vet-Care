34326e2edfa813b2d5488aaac1dca04f8b8984ef
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-28 19:47:51 +0300

    Added job_title to token

--- custom.d.ts
+++ custom.d.ts
@@ -3,6 +3,7 @@ declare namespace Express {
         decodedToken?: {
             id: string,
             permission_type: string,
+            job_title: string,
             iat: number,
             exp: number
         };
