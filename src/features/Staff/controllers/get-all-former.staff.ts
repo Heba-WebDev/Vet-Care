8bfb42922fa8162ee92a9fc2f87f33ef80a9cb64
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-28 19:50:53 +0300

    HR can now access former staff list

--- src/features/Staff/controllers/get-all-former.staff.ts
+++ src/features/Staff/controllers/get-all-former.staff.ts
@@ -7,7 +7,7 @@ const { SUCCESS, FAIL } = statusCode;
 
 const getAllFormerStaff = wrapper(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.decodedToken;
-    if(token?.permission_type !== "Admin") {
+    if(token?.permission_type !== "Admin" || token?.job_title !== "HR") {
         const err = new globalError("Unauthorized to perform this action.", 401
         ,FAIL)
         return next(err);
