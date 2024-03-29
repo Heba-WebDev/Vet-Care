679b4fffbc0f38f524728bd6fdc2441403f96172
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-29 19:54:37 +0300

    Created an object routes to have all app's routes in one palce for better readability

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,14 @@
+import { staffRouter } from "../features/Staff/routes/staff.routes";
+import { vetsRouter } from "../features/Vets/routes/vets.routes";
+import { ownersRouter } from "../features/Owners/routes/owners.routes";
+import { petsRouter } from "../features/Pets/routes/pets.routes";
 
 
+const routes = {
+    staffRouter,
+    vetsRouter,
+    ownersRouter,
+    petsRouter
+}
+
+export { routes }
