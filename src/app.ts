f09c54de4e32f01e56c06f09ae71498ceec44347
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-29 19:50:20 +0300

    Imported routes, an object that has all the routes, for better readability in the file

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import { notFound } from "./middlewares/notFound";
 import { errorHandler } from "./middlewares/errorHandler";
-import { staffRouter } from "./features/Staff/routes/staff.routes";
-import { vetsRouter } from "./features/Vets/routes/vets.routes";
-import { ownersRouter } from "./features/Owners/routes/owners.routes";
+import { routes } from "./routes";
 import config from "./docs/swagger-output.json";
 
 const app = express();
@@ -15,9 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(config));
-app.use("/api/v1/staff", staffRouter);
-app.use("/api/v1/vets", vetsRouter);
-app.use("/api/v1/owners", ownersRouter);
+app.use("/api/v1/staff", routes.staffRouter);
+app.use("/api/v1/vets", routes.vetsRouter);
+app.use("/api/v1/owners", routes.ownersRouter);
+app.use("/api/v1/pets", routes.petsRouter);
 
 app.use(errorHandler);
 app.all("*", notFound)
