013eda1f474ff379efa11f612440ffff48a4e68a
Heba-Webdev23 <jtvonline@yahoo.com>
2024-03-30 21:50:36 +0300

    Created the input validation schema for booking an appointment

---
+++ src/features/Appointments/input-validation/book.appointments.schema.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import { statusCode } from "../../../utils/httpStatusCode";
+import joi from "joi";
+const {FAIL} = statusCode
+
+
+const bookAppointmentsSchema = joi.object({
+  date: joi.date().required().messages({
+    "date.empty": "A valid date is required.",
+    "date.required": "A valid date is required.",
+  }),
+  vet_id: joi
+    .string()
+    .id()
+    .required()
+    .messages({
+      "string.empty": "A valid vet id is required.",
+      "string.id": "A valid vet id is required.",
+    }),
+    owner_id: joi
+    .string()
+    .id()
+    .required()
+    .messages({
+      "string.empty": "A valid owner id is required.",
+      "string.id": "A valid owner id is required.",
+    }),
+    pet_id: joi
+    .string()
+    .id()
+    .required()
+    .messages({
+      "string.empty": "A valid pet id is required.",
+      "string.id": "A valid pet id is required.",
+    }),
+    service_id: joi
+    .string()
+    .id()
+    .required()
+    .messages({
+      "string.empty": "A valid service id is required.",
+      "string.id": "A valid service id is required.",
+    }),
+});
+
+const bookAppointmentsValidation = async (req: Request, res: Response, next: NextFunction) => {
+
+  try {
+    await bookAppointmentsSchema.validateAsync(req.body);
+    next();
+  } catch (error ) {
+    if(error instanceof Error) {
+      res.status(400).send({ status: FAIL, message: error?.message })
+    }
+  }
+};
+
+export { bookAppointmentsValidation };
