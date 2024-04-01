072c5d756862389684bb8f8206f8b0dcb8f8f438
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-02 02:08:11 +0300

    Created the handler for adding an animal type

---
+++ src/features/Animals/controllers/add.animals.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction} from "express";
+import { prisma } from "../../../config/prisma";
+import { wrapper } from "../../../middlewares/asyncWrapper";
+import { statusCode } from "../../../utils/httpStatusCode";
+import { globalError } from "../../../utils/globalError";
+const { SUCCESS, FAIL } = statusCode;
+
+const addAnimals = wrapper(async(req: Request, res: Response, next: NextFunction) => {
+    const {type} = req.body;
+    const token = req.decodedToken;
+    if(token?.permission_type !== "Admin" || (token?.job_title !== "HR" && token?.job_title !== "Manager")) {
+        const err = new globalError("Unauthorized to perform this action.", 401
+        ,FAIL)
+        return next(err);
+    }
+    const animal = await prisma.animals.findFirst({where: {type}});
+    if (animal) {
+        const err = new globalError("Animal type alreadye exist.", 400
+        ,FAIL)
+        return next(err);
+    }
+    await prisma.animals.create({
+        data: {
+            type
+        }
+    }).then(() => {
+        res.status(201).send({
+            status: SUCCESS,
+            message: "Animal type sucesffully added.",
+            data: null
+        });
+    });
+});
+
+export { addAnimals }
