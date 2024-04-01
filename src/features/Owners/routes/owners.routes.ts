35a51a1d14ab81a689233380a22f80fd0854236f
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-02 02:13:14 +0300

    Removed routes getting owner by id and phone number after adding filtering to getAllOwners route

--- src/features/Owners/routes/owners.routes.ts
+++ src/features/Owners/routes/owners.routes.ts
@@ -4,23 +4,17 @@ import { verifyToken } from "../../../middlewares/verifyToken";
 // Controllers
 import { registerOwner } from "../controllers/register.owners";
 import { getAllOwners } from "../controllers/get-all.owners";
-import { getOwnerByNumber } from "../controllers/get-by-number.owners";
-import { getOwnerByName } from "../controllers/get-by-name.owners";
+
 
 // Input validation schemas
 import { ownerRegistrationValidation } from "../input-validation/registration.owners.schema";
-import { ownerByNumberValidation } from "../input-validation/get-by-number.owners.schema";
-import { ownerByNameValidation } from "../input-validation/get-by-name.owners.schema";
+import { getAllOwnersValidation } from "../input-validation/get-all.owners.schema";
 
 const ownersRouter = Router();
 
-ownersRouter.route("/name/:name").get(verifyToken, ownerByNameValidation, getOwnerByName);
-
-ownersRouter.route("/number/:phone_number").get(verifyToken, ownerByNumberValidation, getOwnerByNumber);
-
 ownersRouter.route("/register").post(verifyToken, ownerRegistrationValidation, registerOwner);
 
-ownersRouter.route("/").get(verifyToken, getAllOwners);
+ownersRouter.route("/").get(verifyToken, getAllOwnersValidation, getAllOwners);
 
 
 
