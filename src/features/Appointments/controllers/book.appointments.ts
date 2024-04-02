3e81e67f8911a54d20a2b3b67e0302300b5003bf
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-03 02:23:23 +0300

    Added checking if vet is booked for the same hour

--- src/features/Appointments/controllers/book.appointments.ts
+++ src/features/Appointments/controllers/book.appointments.ts
@@ -6,15 +6,28 @@ import { statusCode } from "../../../utils/httpStatusCode";
 const { SUCCESS, FAIL } = statusCode;
 
 const bookAppointments = wrapper(async(req: Request, res: Response, next: NextFunction) => {
-    const { date, vet_id, owner_id, pet_id, service_id } = req.body;
+    const { date, vet_id, owner_id, pet_id, service_type } = req.body;
+    const d = new Date(date);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    // const d_holiday = new Date(date);
+    // d_holiday.setHours(0);
+    // d_holiday.setMinutes(0);
+    // d_holiday.setSeconds(0);
     const vet = await prisma.veterinarians.findUnique({where: {id: vet_id}});
+    const vet_booked = await prisma.appointments.findFirst({where: {vet_id, date: d}})
     const owner = await prisma.owners.findUnique({where: {id: owner_id}});
     const pet = await prisma.pets.findUnique({where: {id: pet_id}});
-    const service = await prisma.services.findUnique({where: {id: service_id}});
-    const holiday = await prisma.publicHolidays.findFirst({where: {date}});
-    const weekend = new Date(date).getDay();
+    const service = await prisma.services.findUnique({where: {type: service_type}});
+    const holiday = await prisma.publicHolidays.findFirst({where: {date: d}});
     if(!vet) {
         const err = new globalError("No vet found.", 404
+        ,FAIL)
+        return next(err);
+    }
+
+    if(vet_booked) {
+        const err = new globalError("Vet booked for this date and hour.", 400
         ,FAIL)
         return next(err);
     }
@@ -40,18 +53,22 @@ const bookAppointments = wrapper(async(req: Request, res: Response, next: NextFu
         ,FAIL)
         return next(err);
     }
-    if(weekend === 0 || weekend === 6) {
+    if(d.getDay() === 0 || d.getDay() === 6) {
         const err = new globalError("Can not book an appointment on a weekend.", 400
         ,FAIL)
         return next(err);
+    }
+    if (d.getHours() < 8 || d.getHours() > 16) {
+    const err = new globalError("Appointments are only available between 8:00 AM and 5:00 PM.", 400, FAIL);
+    return next(err);
     }
     await prisma.appointments.create({
         data:{
             vet_id,
             owner_id,
             pet_id,
-            date,
-            service_id: service.id
+            date: d,
+            service_type: service.type
         }
     }).then((result) => {
         res.status(201).send({
