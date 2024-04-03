7f4cb0d0fe3145e41ef2c3663caf704a9e5267b5
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-03 23:58:30 +0300

    Added animals' update route

--- src/features/Animals/routes/animals.routes.ts
+++ src/features/Animals/routes/animals.routes.ts
@@ -3,13 +3,16 @@ import { Router } from "express";
 import { verifyToken } from "../../../middlewares/verifyToken";
 // Controllers
 import { addAnimals } from "../controllers/add.animals";
+import { updateAnimal } from "../controllers/update.animals";
 
 // Input validation schemas
 import { animalsRegistrationValidation } from "../input-validation/add.animals.schema";
-
+import { animalsUpdateValidation } from "../input-validation/update.animals.schema";
 
 const animalsRouter = Router();
 
 animalsRouter.route("/").post(verifyToken, animalsRegistrationValidation, addAnimals);
+
+animalsRouter.route("/").patch(verifyToken, animalsUpdateValidation, updateAnimal);
 
 export { animalsRouter}
