d115afe5f4fefa7882fa224152554917bbbe21bb
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-04 00:00:30 +0300

    Added owner update route

--- src/features/Owners/routes/owners.routes.ts
+++ src/features/Owners/routes/owners.routes.ts
@@ -4,11 +4,12 @@ import { verifyToken } from "../../../middlewares/verifyToken";
 // Controllers
 import { registerOwner } from "../controllers/register.owners";
 import { getAllOwners } from "../controllers/get-all.owners";
-
+import { updateOwner } from "../controllers/update.owners";
 
 // Input validation schemas
 import { ownerRegistrationValidation } from "../input-validation/registration.owners.schema";
 import { getAllOwnersValidation } from "../input-validation/get-all.owners.schema";
+import { ownerUpdateValidation } from "../input-validation/update.owners.schema";
 
 const ownersRouter = Router();
 
@@ -16,7 +17,7 @@ ownersRouter.route("/register").post(verifyToken, ownerRegistrationValidation, r
 
 ownersRouter.route("/").get(verifyToken, getAllOwnersValidation, getAllOwners);
 
-
+ownersRouter.route("/").patch(verifyToken, ownerUpdateValidation, updateOwner);
 
 
 export { ownersRouter }
