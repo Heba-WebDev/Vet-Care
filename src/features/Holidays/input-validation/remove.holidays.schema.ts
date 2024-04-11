2d12f041962e0f2b3f9ac4d4a3acc22a172e54dd
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-11 20:15:18 +0300

    Changed id's datatype from string to number to match the database

--- src/features/Holidays/input-validation/remove.holidays.schema.ts
+++ src/features/Holidays/input-validation/remove.holidays.schema.ts
@@ -1,18 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { statusCode } from "../../../utils/httpStatusCode";
 import joi from "joi";
+import { globalError } from "../../../utils/globalError";
 const {FAIL} = statusCode
 
 
 const removeHolidaysSchema = joi.object({
-  id: joi.string().id().required().messages({
-    "string.empty": "A valid holiday id is required.",
-    "string.required": "A valid holiday id is required.",
+  id: joi.number().id().required().messages({
+    "number.empty": "A valid holiday id is required.",
+    "number.required": "A valid holiday id is required.",
   }),
 });
 
 const removeHolidaysValidation = async (req: Request, res: Response, next: NextFunction) => {
-
   try {
     await removeHolidaysSchema.validateAsync(req.body);
     next();
