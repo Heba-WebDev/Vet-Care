8186b70f85703245896ee80983daa083d46fb17d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-11 20:12:47 +0300

    Added duplication checking before adding a holiday. Removed hours, minutes and seconds before adding the holiday

--- src/features/Holidays/controllers/add.holidays.ts
+++ src/features/Holidays/controllers/add.holidays.ts
@@ -7,9 +7,24 @@ const { FAIL, SUCCESS } = statusCode;
 
 const addHoliday = wrapper(async(req: Request, res: Response, next: NextFunction) => {
     const { name, date } = req.body;
+    const date_no_time = new Date(date);
+    date_no_time.setHours(0);
+    date_no_time.setMinutes(0);
+    date_no_time.setSeconds(0);
+    date_no_time.setMilliseconds(0);
     const token = req.decodedToken;
     if((token?.job_title !== "HR" && token?.job_title !== "Manager") || token?.permission_type !== "Admin"){
         const err = new globalError("Unauthorized to perform this action.", 401
+        ,FAIL)
+        return next(err);
+    }
+    const duplicated = await prisma.publicHolidays.findMany({
+        where: {
+            date: date_no_time
+        }
+    });
+    if(duplicated) {
+        const err = new globalError("Holiay already exists.", 400
         ,FAIL)
         return next(err);
     }
