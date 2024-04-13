82e390ac1e9e2c137fa7cf4e9a297c82c9345a80
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-14 00:27:58 +0300

    Made changes to be in sync with the new database schema of holidays

--- src/features/Holidays/controllers/add.holidays.ts
+++ src/features/Holidays/controllers/add.holidays.ts
@@ -7,11 +7,6 @@ const { FAIL, SUCCESS } = statusCode;
 
 const addHoliday = wrapper(async(req: Request, res: Response, next: NextFunction) => {
     const { name, date } = req.body;
-    const date_no_time = new Date(date);
-    date_no_time.setHours(0);
-    date_no_time.setMinutes(0);
-    date_no_time.setSeconds(0);
-    date_no_time.setMilliseconds(0);
     const token = req.decodedToken;
     if((token?.job_title !== "HR" && token?.job_title !== "Manager") || token?.permission_type !== "Admin"){
         const err = new globalError("Unauthorized to perform this action.", 401
@@ -20,7 +15,7 @@ const addHoliday = wrapper(async(req: Request, res: Response, next: NextFunction
     }
     const duplicated = await prisma.publicHolidays.findMany({
         where: {
-            date: date_no_time
+            date
         }
     });
     if(duplicated) {
