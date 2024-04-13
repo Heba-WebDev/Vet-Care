55a909a6d364b937490c3eaa95b12ad0c42eaa2a
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-14 00:26:02 +0300

    Added email notification for the owner once an appointment is cancelled

--- src/features/Appointments/controllers/cancel.appointments.ts
+++ src/features/Appointments/controllers/cancel.appointments.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../../config/prisma";
 import { wrapper } from "../../../middlewares/asyncWrapper";
 import { globalError } from "../../../utils/globalError";
 import { statusCode } from "../../../utils/httpStatusCode";
+import { cancellingNotification } from "../../Notifications/helper-funcs";
 const { SUCCESS, FAIL } = statusCode;
 
 const cancelAppointment = wrapper(async(req: Request, res: Response, next: NextFunction) => {
@@ -13,9 +14,14 @@ const cancelAppointment = wrapper(async(req: Request, res: Response, next: NextF
         ,FAIL)
         return next(err);
     }
-     await prisma.appointments.delete({
+    const owner = await prisma.owners.findUnique({where: {id: appointment.owner_id}});
+    const pet = await prisma.pets.findUnique({where: {id: appointment.pet_id}});
+    const vet = await prisma.veterinarians.findUnique({where: {id: appointment.vet_id}});
+    await prisma.appointments.delete({
         where: {id}
     });
+    // await cancellingNotification(next, {email: owner?.email as string, owner_name: owner?.name as string, vet_name: vet?.name as string, pet_name: pet?.name as string, time: appointment.time, day: appointment.date
+    // });
     res.status(204).send({
             status: SUCCESS,
             message: "Appointment succesfully cancelled.",
