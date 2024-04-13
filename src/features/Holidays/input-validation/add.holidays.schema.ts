f9036a226d8227c287fc37b493ed9f13ab23d5f6
Heba-Webdev23 <jtvonline@yahoo.com>
2024-04-14 00:28:56 +0300

    Changed date from date to string type and updated the error messages accordingly

--- src/features/Holidays/input-validation/add.holidays.schema.ts
+++ src/features/Holidays/input-validation/add.holidays.schema.ts
@@ -6,9 +6,11 @@ const {FAIL} = statusCode
 
 
 const addHolidaysSchema = joi.object({
-  date: joi.date().required().messages({
-    "date.empty": "A valid holiday date is required.",
-    "date.required": "A valid holiday date is required.",
+  date: joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/).required().messages({
+    "string.empty": "A valid date is required.",
+    "string.required": "A valid date is required.",
+    "string.pattern.base": "A valid date of format yyyy:mm:dd is required.",
+    "string.pattern": "A valid date of format yyyy:mm:dd is required."
   }),
   name: joi
     .string()
