542feb071cb846297dd285b68f4b9610d4f017de
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-26 02:30:47 +0300

    domain index file

---
+++ src/domain/index.ts
@@ -0,0 +1,11 @@
+
+
+export * from './dtos/staff/register.staff.dto';
+
+export * from './entities/staff.entity';
+
+export * from './errors/custom.error';
+
+export * from './datasources/staff.datasource';
+
+export * from './repositories/staff.repository';
