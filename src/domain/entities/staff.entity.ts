e333ab6aee83f33ce9b4a58f7af1b4a8fab3eae5
Heba-Webdev23 <jtvonline@yahoo.com>
2024-06-26 02:31:54 +0300

    staff entity

---
+++ src/domain/entities/staff.entity.ts
@@ -0,0 +1,15 @@
+
+
+export class StaffEntity {
+    constructor(
+        public id: string,
+        public name: string,
+        public job_title: string,
+        public permission_type:string,
+        public email: string,
+        public password: string,
+        public phone_number: string,
+        public verified: boolean,
+    ) {}
+
+}
