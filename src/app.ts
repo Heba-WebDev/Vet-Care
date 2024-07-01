a6ac3904806a1387d8d676529cdc1d1b446b80cb
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-02 01:22:35 +0300

    feat: created app's main entry point

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { envs } from "./config";
+import { AppRoutes, Server } from "./presentation";
+
+
+// IIFE
+(() => {
+    main();
+})()
+
+{/*
+    The advatnage of calling main in an immediately invoked Function
+    is that now I can await my database to connect, for example.
+*/}
+async function main () {
+    new Server({
+        port: envs.PORT
+    }).start();
+}
+
