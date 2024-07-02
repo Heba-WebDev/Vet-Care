53d20f9c3f747c96446bfe4f296cec19864a9581
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:31:36 +0300

    feat: update staff validation schema

---
+++ src/features/Staff/infrastructure/validation/joi-schemas/update-staff.schema.ts
@@ -0,0 +1,43 @@
+import joi from 'joi';
+
+export const updateSchema = joi.object({
+  id: joi
+    .string()
+    .id()
+    .required()
+    .messages({
+      "string.empty": "A valid id is required.",
+      "string.id": "A valid id is required.",
+    }),
+    email: joi
+    .string()
+    .email()
+    .optional()
+    .messages({
+      "string.empty": "A valid email is required.",
+      "string.email": "A valid email is required.",
+      "string.optional": "A valid email is required.",
+    }),
+  password: joi
+    .string()
+    .min(6)
+    .pattern(new RegExp("^[a-zA-Z0-9]{6,30}$"))
+    .optional()
+    .messages({
+      "string.empty": "A valid password is required.",
+      "string.required": "A valid password is required.",
+      "string.min": "Password must have at least 6 characters.",
+      "string.pattern": "A password must be of 6 characters or more.",
+      "string.optional": "A valid password is required.",
+    }),
+    phone_number: joi.string().pattern(new RegExp("^[\\d\\s-]*$")).min(6).optional().messages({
+      "string.empty": "A valid phone number is required.",
+      "string.required": "A valid phone number is required.",
+      "string.pattern": "A phone number can only contain digits, - or white space.",
+      "string.optional": "A valid phone number is required.",
+    }),
+    job_title: joi.string().optional().messages({
+      "string.empty": "A valid job title is required.",
+      "string.optional": "A valid job title is required."
+    })
+});
