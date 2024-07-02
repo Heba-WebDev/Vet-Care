02a72a1691cd9c94ec50aaed43e802e7542f9b8f
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:21:28 +0300

    feat: added login and verify abstracts

--- src/features/Staff/domain/datasources/staff.datasource.ts
+++ src/features/Staff/domain/datasources/staff.datasource.ts
@@ -1,7 +1,9 @@
-import { RegisterStaffDto } from "../dtos";
+import { LoginStaffDto, RegisterStaffDto, VerifyStaffDto } from "../dtos";
 import { StaffEntity } from "../entities/staff.entity";
 
 // abstract to avoid creation of new instances
 export abstract class StaffDatasource {
-    abstract register(staffDto: RegisterStaffDto):Promise<StaffEntity | null>
+    abstract register(staffDto: RegisterStaffDto):Promise<StaffEntity | null>;
+    abstract login(staffDto: LoginStaffDto):Promise<StaffEntity | null>;
+    abstract verify(staffDto: VerifyStaffDto):Promise<StaffEntity | null>;
 }
