c58a59d312a88cd156fc21abcc2e270c341efae4
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:25:20 +0300

    feat: added login and verify abstractions

--- src/features/Staff/domain/repositories/staff.repository.ts
+++ src/features/Staff/domain/repositories/staff.repository.ts
@@ -1,7 +1,9 @@
-import { RegisterStaffDto } from "../dtos";
+import { LoginStaffDto, RegisterStaffDto, VerifyStaffDto } from "../dtos";
 import { StaffEntity } from "../entities/staff.entity";
 
 // abstract to avoid creation of new instances
 export abstract class StaffRepository {
     abstract register(staffDto: RegisterStaffDto):Promise<StaffEntity | null>
+    abstract login(StaffDto: LoginStaffDto):Promise<StaffEntity | null>;
+    abstract verify(staffDto: VerifyStaffDto):Promise<StaffEntity | null>;
 }
