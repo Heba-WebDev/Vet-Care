aeeb15e28ac8ec3e2eab9e78a50416c963d0f31c
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:30:54 +0300

    feat: validaiton schemas index file

---
+++ src/features/Staff/infrastructure/validation/joi-schemas/index.ts
@@ -0,0 +1,7 @@
+
+export * from './register-staff.schema'
+export * from './login-staff.schema'
+export * from './delete-staff.schema'
+export * from './get-staff.schema'
+export * from './update-staff.schema'
+export * from './verify-staff.schema'
