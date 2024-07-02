a722ca42f14c8cdf8d51b90d034d01a5f5b86b40
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-02 19:49:46 +0300

    feat: staff register dto

---
+++ src/features/Staff/domain/dtos/register-staff.dto.ts
@@ -0,0 +1,23 @@
+import { StaffInputValidation } from "../../infrastructure/validation";
+
+
+export class RegisterStaffDto {
+
+    private constructor(
+        public name: string,
+        public job_title: string,
+        public email: string,
+        public password: string,
+        public phone_number: string,
+        )
+    {
+    }
+
+    static create(object: {[key: string]: any}): [string?, RegisterStaffDto?] {
+        const { name, job_title, email, password, phone_number } = object;
+        const staffDto = new RegisterStaffDto(name, job_title, email, password, phone_number);
+        const err = new StaffInputValidation().register(staffDto);
+        if (err) return [err, undefined]
+        return [undefined, staffDto]
+    }
+}
