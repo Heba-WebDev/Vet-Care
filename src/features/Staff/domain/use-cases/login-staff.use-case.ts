62d2dfc2062fb21eea0c65ab4d58c615b5e8dac6
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:26:32 +0300

    feat: login staff use case

---
+++ src/features/Staff/domain/use-cases/login-staff.use-case.ts
@@ -0,0 +1,28 @@
+import { JwtAdapter } from "../../../../config";
+import { CustomError } from "../../../../domain";
+import { LoginStaffDto } from "../dtos/login-staff.dto";
+import { LoginStaffUseCase, SignToken } from "../interfaces";
+import { StaffRepository } from "../repositories";
+
+
+export class LoginStaff implements LoginStaffUseCase {
+
+    constructor(
+        private readonly repo: StaffRepository,
+        private readonly signToken: SignToken = JwtAdapter.generateToken,
+    ) {}
+
+    async execute(loginStaff: LoginStaffDto): Promise<any> {
+        const staff = await this.repo.login(loginStaff);
+        const token = await this.signToken({
+            id: staff?.id!,
+            permission_type: staff?.permission_type!,
+            job_title: staff?.job_title! });
+        if (!token) throw CustomError.internalServerError('Internal server error');
+        return {
+            token,
+            user: staff
+        }
+    }
+}
+
