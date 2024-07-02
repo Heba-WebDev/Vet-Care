eb0e922a2af6d5f65b0f4f6422a063d055282d6c
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:29:47 +0300

    feat: added verify and delete validation

--- src/features/Staff/infrastructure/validation/staff.validation.impl.ts
+++ src/features/Staff/infrastructure/validation/staff.validation.impl.ts
@@ -1,12 +1,33 @@
 import { RegisterStaffDto } from "../../domain";
+import { DeleteStaffDto } from "../../domain/dtos/delete-staff.dto";
+import { LoginStaffDto } from "../../domain/dtos/login-staff.dto";
+import { VerifyStaffDto } from "../../domain/dtos/verify-staff.dto";
 import { StaffValidation } from "../../domain/validation/register-staff";
-import { registerSchema } from "./joi-schemas/register-staff.schema";
+import { registerSchema, loginSchema } from "./joi-schemas";
+import { verifySchema } from "./joi-schemas/verify-staff.schema";
+
 
 
 export class StaffInputValidation implements StaffValidation {
     register(staffDto: RegisterStaffDto): string | null {
-    const { error } = registerSchema.validate(staffDto)
+    const { error } = registerSchema.validate(staffDto);
     if (error) return error.message;
     return null;
+    }
+
+    login(staffDto: LoginStaffDto): string | null {
+        const { error } = loginSchema.validate(staffDto);
+        if (error) return error.message;
+        return null;
+    }
+
+    verify(staffDto: VerifyStaffDto): string | null {
+        const { error } = verifySchema.validate(staffDto);
+        return null;
+    }
+
+    delete(staffDto: DeleteStaffDto): string | null {
+        const { error } = verifySchema.validate(staffDto);
+        return null;
     }
 }
