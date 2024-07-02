1da15e4a519299e505280f7ee04d10ddf8762a51
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:28:38 +0300

    feat: added login and verify implementation

--- src/features/Staff/infrastructure/datasources/staff.datasource.impl.ts
+++ src/features/Staff/infrastructure/datasources/staff.datasource.impl.ts
@@ -1,6 +1,7 @@
+import { bcryptAdapter } from "../../../../config";
 import { prisma } from "../../../../data";
 import { CustomError } from "../../../../domain";
-import { RegisterStaffDto, StaffDatasource } from "../../domain";
+import { LoginStaffDto, RegisterStaffDto, StaffDatasource, VerifyStaffDto } from "../../domain";
 import { StaffEntity } from "../../domain/entities";
 import { StaffMapper } from "../mapper";
 
@@ -10,18 +11,58 @@ export class StaffDatasourceImpl implements StaffDatasource {
         const { name, email, password, phone_number, job_title } = staffDto;
         try {
             const exists = await prisma.staff.findFirst({where: { email }});
-            
-            if (exists) throw CustomError.badRequest('Email already exits');
+            if (exists) throw CustomError.badRequest('Provide a different email');
             const job = await prisma.jobs.findFirst({where: {title: job_title}});
-            
-            if (!job) throw CustomError.badRequest('Please provide a valid job title [Receptionist, HR, or Manager].');
+            if (!job) throw CustomError.badRequest('Provide a valid job title [Receptionist, HR, or Manager]');
+            const phoneExists = await prisma.staff.findFirst({where: {phone_number}});
+            if (phoneExists) throw CustomError.badRequest('Provide a different phone number');
+            const hashedPassword = bcryptAdapter.hash(password);
 
             const staff = await prisma.staff.create({
                 data: {
-                    name, email, password, phone_number, job_title
+                    name, email, password: hashedPassword, phone_number, job_title
                 }
             });
             return StaffMapper.staffEntityFromObject(staff);
+
+        }catch(error) {
+            if (error instanceof CustomError) throw error;
+            throw CustomError.internalServerError()
+        }
+    }
+
+    async login(staffDto: LoginStaffDto): Promise<StaffEntity | null> {
+        const { email, password } = staffDto;
+        try {
+            const exists = await prisma.staff.findFirst({where: { email }});
+
+            if (!exists) throw CustomError.badRequest('Invalid credentionals');
+            const passwordMatch = bcryptAdapter.compare(password, exists.password!);
+            if (!passwordMatch) throw CustomError.badRequest('Invalid credentionals');
+            const staff = await prisma.staff.findFirst({
+                where: {
+                    email
+                }
+            });
+            return StaffMapper.staffEntityFromObject(staff!);
+
+        }catch(error) {
+            if (error instanceof CustomError) throw error;
+            throw CustomError.internalServerError()
+        }
+    }
+
+    async verify(staffDto: VerifyStaffDto): Promise<StaffEntity | null> {
+        const { email } = staffDto;
+        try {
+            const exists = await prisma.staff.findFirst({where: { email }});
+            if (!exists) throw CustomError.badRequest('Invalid credentionals');
+            if (exists.verified) throw CustomError.badRequest('Staff member already verified');
+            const staff = await prisma.staff.update({
+                data: {verified: true},
+                where: {email}
+            });
+            return StaffMapper.staffEntityFromObject(staff!);
 
         }catch(error) {
             if (error instanceof CustomError) throw error;
