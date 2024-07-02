7aa8313f16766d0becee33c4ec016054a734bda9
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-03 02:21:53 +0300

    feat: delete staff dto

---
+++ src/features/Staff/domain/dtos/delete-staff.dto.ts
@@ -0,0 +1,17 @@
+import { StaffInputValidation } from "../../infrastructure/validation";
+
+export class DeleteStaffDto{
+
+    private constructor(
+        public id: string,
+        public exit_reason: string,
+    ) {}
+
+    static delete(object: {[key: string]: any}): [string?, DeleteStaffDto?] {
+        const { id, exit_reason } = object;
+        const staffDto = new DeleteStaffDto(id, exit_reason);
+        const err = new StaffInputValidation().delete(staffDto);
+        if (err) return [err, undefined]
+        return [undefined, staffDto];
+    }
+}
