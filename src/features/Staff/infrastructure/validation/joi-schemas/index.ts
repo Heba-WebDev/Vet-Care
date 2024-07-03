9a443529aa72e96bbfe2d13c7851073e5855110c
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-04 01:13:42 +0300

    feat: updated the exported schemas

--- src/features/Staff/infrastructure/validation/joi-schemas/index.ts
+++ src/features/Staff/infrastructure/validation/joi-schemas/index.ts
@@ -2,6 +2,6 @@
 export * from './register-staff.schema'
 export * from './login-staff.schema'
 export * from './delete-staff.schema'
-export * from './get-staff.schema'
 export * from './update-staff.schema'
 export * from './verify-staff.schema'
+export * from './get-all-staff.schema'
