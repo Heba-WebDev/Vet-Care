ed6c250f57bfbc263d8dc18ad40d84391c36fd46
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-04 01:05:24 +0300

    feat: get-all-staff dto

---
+++ src/features/Staff/domain/dtos/get-staff.dto.ts
@@ -0,0 +1,19 @@
+import { StaffInputValidation } from "../../infrastructure/validation";
+
+
+export class GetAllStaffDto {
+
+    constructor(
+        public readonly page?: number,
+        public readonly limit?: number,
+    ) {}
+
+    static get(object: {[key: string]: any}): [string?, GetAllStaffDto?] {
+        const page = parseInt(object.page) || 1;
+        const limit = parseInt(object.limit) || 15;
+        const staffDto = new GetAllStaffDto(page, limit);
+        const err = new StaffInputValidation().getAll(staffDto);
+        if (err) return [err, undefined]
+        return [undefined, staffDto]
+    }
+}
