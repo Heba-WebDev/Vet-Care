4bf374714f7a787348446329767068576a8da219
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-04 01:04:51 +0300

    feat: added delete, getAll and getAllFormer abstractions

--- src/features/Staff/domain/datasources/staff.datasource.ts
+++ src/features/Staff/domain/datasources/staff.datasource.ts
@@ -1,4 +1,5 @@
-import { LoginStaffDto, RegisterStaffDto, VerifyStaffDto } from "../dtos";
+import { LoginStaffDto, RegisterStaffDto, VerifyStaffDto, DeleteStaffDto, GetAllStaffDto } from "../dtos";
+import { FormerStaffEntity } from "../entities/former-staff.entity";
 import { StaffEntity } from "../entities/staff.entity";
 
 // abstract to avoid creation of new instances
@@ -6,4 +7,7 @@ export abstract class StaffDatasource {
     abstract register(staffDto: RegisterStaffDto):Promise<StaffEntity | null>;
     abstract login(staffDto: LoginStaffDto):Promise<StaffEntity | null>;
     abstract verify(staffDto: VerifyStaffDto):Promise<StaffEntity | null>;
+    abstract delete(staffDto: DeleteStaffDto):Promise<StaffEntity | null>;
+    abstract getAll(staffDto: GetAllStaffDto):Promise<StaffEntity[] | null>;
+    abstract getAllFormer(staffDto: GetAllStaffDto):Promise<FormerStaffEntity[] | null>;
 }
