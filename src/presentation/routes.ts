59ecff346f6649835a545d465c779ac70ccda0c3
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-08 13:35:46 +0300

    feat: added vets router

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { StaffRoutes } from '../features/Staff';
+import { VetsRoutes } from '../features/Vets/presentation';
 
 
 export class AppRoutes {
@@ -7,6 +8,7 @@ export class AppRoutes {
     static get routes(): Router {
         const router = Router();
         router.use('/api/v2/staff', StaffRoutes.routes);
+        router.use('/api/v2/vets', VetsRoutes.routes);
         return router;
     }
 }
