e116bdf5b9a12dce0f80a632846bcaa0b7780b77
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-09 11:28:08 +0300

    feat: vet mocks

---
+++ src/features/Vets/__tests__/mocks/vet.mock.ts
@@ -0,0 +1,32 @@
+import { RegisterVetsDto } from "../../domain";
+import { VetEntity } from "../../domain/entities";
+
+
+export const vetRegisterDtoMock: RegisterVetsDto = {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    password: 'password123',
+    phone_number: '1234567890',
+    job_title: 'Veterinarian'
+}
+
+
+export const vetEntityVerifiedMock = new VetEntity(
+    '1',
+    vetRegisterDtoMock.name,
+    vetRegisterDtoMock.job_title,
+    'Veterinarian',
+    vetRegisterDtoMock.email,
+    vetRegisterDtoMock.phone_number,
+    true
+);
+
+export const vetEntityUnveriviedMock = new VetEntity(
+    '1',
+    vetRegisterDtoMock.name,
+    vetRegisterDtoMock.job_title,
+    'Veterinarian',
+    vetRegisterDtoMock.email,
+    vetRegisterDtoMock.phone_number,
+    false
+);
