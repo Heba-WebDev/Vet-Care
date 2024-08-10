d3f0d6f4e9ea1b811400292579540b9a81c719b3
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-10 11:26:36 +0300

    feat: added login

--- src/features/Vets/domain/repositories/vets.repository.ts
+++ src/features/Vets/domain/repositories/vets.repository.ts
@@ -1,8 +1,8 @@
-import { RegisterVetsDto, VerifyVetDto } from "../dtos";
+import { RegisterVetsDto, VerifyVetDto, LoginVetsDto } from "../dtos";
 import { VetEntity } from "../entities";
-
 
 export abstract class VetsRepository {
     abstract register(vetsDto: RegisterVetsDto): Promise<VetEntity | null>
     abstract verify(vetsDto: VerifyVetDto): Promise<VetEntity | null>
+    abstract login(vetsDto: LoginVetsDto): Promise<VetEntity | null>
 }
