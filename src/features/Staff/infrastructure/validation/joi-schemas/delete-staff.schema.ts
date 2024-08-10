4450c00a9fb6e48aee5ef4d1ee6ee1b82774a78d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-10 14:00:44 +0300

    feat: updated schema

--- src/features/Staff/infrastructure/validation/joi-schemas/delete-staff.schema.ts
+++ src/features/Staff/infrastructure/validation/joi-schemas/delete-staff.schema.ts
@@ -3,15 +3,16 @@ import joi from 'joi';
 export const deleteSchema = joi.object({
   id: joi
     .string()
-    .id()
+    .uuid()
     .required()
     .messages({
       "string.empty": "A valid id is required.",
-      "string.id": "A valid id is required.",
+      "string.required": "A valid id is required.",
+      "string.uuid": "A valid id is required.",
     }),
-  exit_reason: joi.string().min(5).required().messages({
+  exit_reason: joi.string().min(5).max(50).optional().messages({
     "string.empty": "A valid exit reason is required.",
-    "string.required": "A valid exit reason is required.",
-    "string.min": "Please provide a valid exit reason."
+    "string.min": "An exit reason must be more than 5 characters.",
+    "string.max": "An exit reason must be less than 50 characters."
   })
 });
