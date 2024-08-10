6859a14c3a4d9241db47b1807d0bbee26e31de6d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-10 11:29:35 +0300

    feat: added login controller

--- src/features/Vets/presentation/controller.vets.ts
+++ src/features/Vets/presentation/controller.vets.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import { BaseController } from "../../../presentation/base.controller";
-import { RegisterVets } from "../domain/use-cases";
+import { LoginVets, RegisterVets } from "../domain/use-cases";
 import { VetsRepository } from "../domain/repositories";
 import { VerifyVets } from "../domain/use-cases/verify-vets.use-case";
-import { RegisterVetsDto, VerifyVetDto } from "../domain";
+import { RegisterVetsDto, VerifyVetDto, LoginVetsDto } from "../domain";
 
 
 export class VetsController extends BaseController {
@@ -30,7 +30,11 @@ export class VetsController extends BaseController {
     }
 
     login = (req: Request, res: Response) => {
-
+        const [error, loginDto] = LoginVetsDto.login(req.body);
+        if (error) return res.status(400).send({ error });
+        new LoginVets(this.vetsRepo).execute(loginDto!)
+        .then((data) => res.json(data))
+        .catch((error) => this.handleError(error, res))
     }
 
     update = (req: Request, res: Response) => {}
