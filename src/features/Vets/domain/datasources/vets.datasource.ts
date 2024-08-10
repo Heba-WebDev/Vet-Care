94f59e8bd6abbc903bdbfba40e0c56c2aea5b082
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-10 11:25:27 +0300

    feat: added login

--- src/features/Vets/domain/datasources/vets.datasource.ts
+++ src/features/Vets/domain/datasources/vets.datasource.ts
@@ -1,7 +1,8 @@
-import { RegisterVetsDto, VerifyVetDto } from "../dtos";
+import { RegisterVetsDto, VerifyVetDto, LoginVetsDto } from "../dtos";
 import { VetEntity } from "../entities";
 
 export abstract class VetsDatasource {
-    abstract register(vetsDto: RegisterVetsDto): Promise<VetEntity | null>;
-    abstract verify(vetsDto: VerifyVetDto): Promise<VetEntity | null>;
+    abstract register(vetsDto: RegisterVetsDto): Promise<VetEntity | null>
+    abstract verify(vetsDto: VerifyVetDto): Promise<VetEntity | null>
+    abstract login(vetsDto: LoginVetsDto): Promise<VetEntity | null>
 }
