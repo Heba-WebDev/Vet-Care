6178614dc5ed519407f090dfe83fe6c63d05c465
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-12 14:35:59 +0300

    fix: removed name

--- src/features/Staff/domain/dtos/update-staff.dto.ts
+++ src/features/Staff/domain/dtos/update-staff.dto.ts
@@ -5,16 +5,15 @@ export class UpdateStaffDto {
 
     private constructor(
         public id: string,
-        public name: string,
         public email: string,
         public password: string,
         public phone_number: string,
     ) {}
 
     static upate(object: {[key: string]: any}): [string?, UpdateStaffDto?] {
-        const { id, name, email, password, phone_number } = object;
-        if (!name && !email && !password && !phone_number ) return ['Provide an email, a password or a phone number to update', undefined]
-        const staffDto = new UpdateStaffDto(id, name, email, password, phone_number);
+        const { id, email, password, phone_number } = object;
+        if (!email && !password && !phone_number ) return ['Provide an email, a password or a phone number to update', undefined]
+        const staffDto = new UpdateStaffDto(id, email, password, phone_number);
         const err = new StaffInputValidation().update(staffDto);
         if (err) return [err, undefined]
         return [undefined, staffDto]
