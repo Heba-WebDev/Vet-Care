4929d0f0a249c960285141fd5abb21b7a178650b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-12 14:47:18 +0300

    feat: update vet dto

---
+++ src/features/Vets/domain/dtos/update-vets.dto.ts
@@ -0,0 +1,22 @@
+import { VetsInputValidation } from "../../infrastructure";
+
+
+export class UpdateVetsDto {
+
+    private constructor(
+        public id: string,
+        public email: string,
+        public password: string,
+        public phone_number: string,
+    ) {}
+
+    static upate(object: {[key: string]: any}): [string?, UpdateVetsDto?] {
+        const { id, email, password, phone_number } = object;
+        if (!email && !password && !phone_number ) return ['Provide an email, a password or a phone number to update', undefined]
+        const vetsDto = new UpdateVetsDto(id, email, password, phone_number);
+        const err = new VetsInputValidation().update(vetsDto);
+        if (err) return [err, undefined]
+        return [undefined, vetsDto]
+    }
+
+}
