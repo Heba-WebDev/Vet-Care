df7112d5383cc46b98059d969726371587b0b693
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-20 14:28:12 +0300

    feat: register owner use case

---
+++ src/features/Owners/domain/use-cases/register-owner.use-case.ts
@@ -0,0 +1,17 @@
+import { RegisterOwnerDto } from "../dtos";
+import { OwnersStandardResponse, RegisterOwnerUseCase } from "../interfaces";
+import { OwnersRepository } from "../repositories";
+
+export class RegisterOwner implements RegisterOwnerUseCase {
+    constructor(
+        private readonly repo: OwnersRepository
+    ) {}
+    public async execute(registerOwner: RegisterOwnerDto): Promise<OwnersStandardResponse> {
+        const owner = await this.repo.register(registerOwner);
+        return {
+            status: 'success',
+            message: 'Owner successfully registered',
+            data: owner
+        }
+    }
+}
