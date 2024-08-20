57f53197ae5d43ad9af142196e6b79e9fa0152a1
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-20 16:28:42 +0300

    feat: get-all owners

--- src/features/Owners/domain/datasources/owners.datasource.ts
+++ src/features/Owners/domain/datasources/owners.datasource.ts
@@ -1,6 +1,7 @@
-import { RegisterOwnerDto } from "../dtos";
+import { RegisterOwnerDto, GetAllOwnersDto } from "../dtos";
 import { OwnerEntity } from "../entities";
 
 export abstract class OwnersDatasource {
     abstract register(ownersDto: RegisterOwnerDto): Promise<OwnerEntity | null>;
+    abstract getAll(ownerDto: GetAllOwnersDto | null): Promise<OwnerEntity[] | null>;
 }
--- src/features/Owners/domain/repositories/owners.repository.ts
+++ src/features/Owners/domain/repositories/owners.repository.ts
@@ -1,6 +1,7 @@
-import { RegisterOwnerDto } from "../dtos";
+import { RegisterOwnerDto, GetAllOwnersDto } from "../dtos";
 import { OwnerEntity } from "../entities";
 
 export abstract class OwnersRepository {
     abstract register(ownersDto: RegisterOwnerDto): Promise<OwnerEntity | null>;
+    abstract getAll(ownerDto: GetAllOwnersDto | null): Promise<OwnerEntity[] | null>;
 }
