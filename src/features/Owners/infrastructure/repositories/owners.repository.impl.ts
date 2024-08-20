9f2d216360a315ee0a28f6f34056513532899be9
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-20 23:28:09 +0300

    feat: updated returned promise type

--- src/features/Owners/infrastructure/repositories/owners.repository.impl.ts
+++ src/features/Owners/infrastructure/repositories/owners.repository.impl.ts
@@ -1,4 +1,10 @@
-import { OwnerEntity, OwnersDatasource, RegisterOwnerDto, GetAllOwnersDto } from "../../domain";
+import {
+    OwnerEntity,
+    OwnersDatasource,
+    RegisterOwnerDto,
+    GetAllOwnersDto,
+    AllOwnersDatasourceResponse
+} from "../../domain";
 import { OwnersRepository } from "../../domain/repositories"
 
 export class OwnersRepositoryImpl extends OwnersRepository {
@@ -12,7 +18,7 @@ export class OwnersRepositoryImpl extends OwnersRepository {
         return this.datasource.register(ownerDto);
     }
 
-    getAll(ownerDto: GetAllOwnersDto): Promise<OwnerEntity[] | null> {
+    getAll(ownerDto: GetAllOwnersDto): Promise<AllOwnersDatasourceResponse | null> {
         return this.datasource.getAll(ownerDto);
     }
 }
