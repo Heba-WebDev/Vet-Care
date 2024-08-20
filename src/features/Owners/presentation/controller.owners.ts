2adef112f6b66dcedfaef3ec051f48258402a228
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-20 16:32:11 +0300

    feat: get-all owners

--- src/features/Owners/presentation/controller.owners.ts
+++ src/features/Owners/presentation/controller.owners.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-import { OwnersRepository, RegisterOwnerDto } from '../domain';
-import { RegisterOwner } from '../domain/use-cases';
+import { OwnersRepository, RegisterOwnerDto, GetAllOwnersDto } from '../domain';
+import { RegisterOwner, GetAllOwners } from '../domain/use-cases';
 import { BaseController } from '../../../presentation/base.controller';
 
 export class OwnersController extends BaseController {
@@ -14,6 +14,14 @@ export class OwnersController extends BaseController {
         const [error, dto] = RegisterOwnerDto.register(req.body);
         if (error) return res.status(400).send({ error });
         new RegisterOwner(this.repo).execute(dto!)
+        .then((data) => res.json(data))
+        .catch((error) => this.handleError(error, res))
+    }
+
+    getAll = (req: Request, res: Response)  => {
+        const [error, dto]= GetAllOwnersDto.getAll(req.query);
+        if (error) return res.status(400).send({ error });
+        new GetAllOwners(this.repo).execute(dto!)
         .then((data) => res.json(data))
         .catch((error) => this.handleError(error, res))
     }
