5f50b9181faa223db2d818703d7c88af6da960d0
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-20 14:36:33 +0300

    feat: owners router

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { StaffRoutes } from '../features/Staff';
 import { VetsRoutes } from '../features/Vets/presentation';
-
+import { OwnersRoutes } from '../features/Owners';
 
 export class AppRoutes {
 
@@ -9,6 +9,7 @@ export class AppRoutes {
         const router = Router();
         router.use('/api/v2/staff', StaffRoutes.routes);
         router.use('/api/v2/vets', VetsRoutes.routes);
+        router.use('/api/v2/owners', OwnersRoutes.routes);
         return router;
     }
 }
