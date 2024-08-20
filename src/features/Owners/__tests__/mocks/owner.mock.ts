5308f7319470fc09f0838c7f5242182116f4e242
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-20 16:27:56 +0300

    tests: get-all owners mocks

--- src/features/Owners/__tests__/mocks/owner.mock.ts
+++ src/features/Owners/__tests__/mocks/owner.mock.ts
@@ -1,4 +1,4 @@
-import { RegisterOwnerDto } from "../../domain";
+import { RegisterOwnerDto, GetAllOwnersDto, OwnerEntity } from "../../domain";
 
 export const registerOwnerDtoMock: RegisterOwnerDto = {
     name: 'John',
@@ -12,6 +12,22 @@ export const ownerMock = {
     email: registerOwnerDtoMock.email,
     phone_number: registerOwnerDtoMock.phone_number
 };
+
+export const secondOwnerMock = {
+    id: 'bd9f9bbc-b25a-4b90-a60a-ef7ba589a2b3',
+    name: 'Jack',
+    email: 'jack@example.com',
+    phone_number: '0434527987'
+}
+
+export const getAllOwnersDtoMock: GetAllOwnersDto = {
+    id: ownerMock.id,
+    name: 'John',
+    email: 'john@example.com',
+    phone_number: '068226266'
+};
+
+export const AllOwnersMock: OwnerEntity[] = [ownerMock, secondOwnerMock];
 
 export const ownerMapped = {
     name: registerOwnerDtoMock.name,
