077cd81a0ef6b9ae61dc538da6cff952f1d175ed
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-22 23:08:52 +0300

    chore: seeded data in animal

---
+++ src/data/prisma/enums/index.ts
@@ -0,0 +1,40 @@
+export enum Animal {
+  Cat,
+  Dog,
+  Horse,
+  Bird,
+  Snake,
+  Lizard,
+  Hamster,
+  Rat,
+  Rabbit,
+}
+
+export enum Permission {
+  Staff,
+  Admin,
+}
+
+export enum Title {
+  Receptionist,
+  HR,
+  Manager,
+  Veterinarian,
+  Asistant,
+  Technician,
+}
+
+export enum Status {
+  Paid,
+  Unpaid
+}
+
+export enum Method {
+  Cash,
+  Online
+}
+
+export enum Gender {
+  Male,
+  Female
+}
---
+++ src/data/prisma/seed/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaClient, Animal } from "@prisma/client";
+import { logger } from "../../../infrastructure";
+
+class AnimalSeeder {
+  private prisma: PrismaClient;
+
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+
+  async seed() {
+    const animalTypes: { type: Animal }[] = [
+      { type: Animal.Cat },
+      { type: Animal.Dog },
+      { type: Animal.Horse },
+      { type: Animal.Bird },
+      { type: Animal.Snake },
+      { type: Animal.Lizard },
+      { type: Animal.Hamster },
+      { type: Animal.Rat },
+      { type: Animal.Rabbit },
+    ];
+
+    for (const animal of animalTypes) {
+      await this.prisma.animals.upsert({
+        where: { type: animal.type as Animal },
+        update: {},
+        create: animal,
+      });
+    }
+
+    logger.info('Animal types have been seeded.');
+  }
+}
+
+const prisma = new PrismaClient();
+const seeder = new AnimalSeeder(prisma);
+
+seeder.seed()
+  .catch(e => {
+    logger.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
