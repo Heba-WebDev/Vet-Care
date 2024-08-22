5205f26c5b06e5c14d1c7f898bebe0566ff223ce
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-22 16:03:50 +0300

    feat: update owner validation schema

--- src/features/Owners/infrastructure/validation/owners.validation.impl.ts
+++ src/features/Owners/infrastructure/validation/owners.validation.impl.ts
@@ -1,5 +1,5 @@
-import { RegisterOwnerDto, GetAllOwnersDto } from "../../domain";
-import { ownerRegisterSchema, getAllOwnersSchema } from "./joi-schemas";
+import { RegisterOwnerDto, GetAllOwnersDto, UpdateOwnerDto } from "../../domain";
+import { ownerRegisterSchema, getAllOwnersSchema, ownerUpdateSchema } from "./joi-schemas";
 
 export class OwnersInputValidation {
 
@@ -11,6 +11,12 @@ export class OwnersInputValidation {
 
     getAll(ownerDto: GetAllOwnersDto): string | null {
         const { error } = getAllOwnersSchema.validate(ownerDto);
+        if (error) return error.message;
+        return null;
+    }
+
+    update(ownerDto: UpdateOwnerDto): string | null {
+        const { error } = ownerUpdateSchema.validate(ownerDto);
         if (error) return error.message;
         return null;
     }
