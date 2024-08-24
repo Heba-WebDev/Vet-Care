c60c17a4df5ca986880e5032c370e7c924f2f776
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-24 18:15:17 +0300

    feat: prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,20 +3,20 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-  {
-    ignores: ['dist', 'node_modules']
-  },
-  { files: ['**/*.{ts}'] },
-  {
-    languageOptions: { globals: globals.node },
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      'prefer-const': 'error',
-      'no-unused-vars': 'error'
-    }
-  },
+    {
+        ignores: ['dist', 'node_modules'],
+    },
+    { files: ['**/*.{ts}'] },
+    {
+        languageOptions: { globals: globals.node },
+        rules: {
+            semi: ['error', 'always'],
+            quotes: ['error', 'single'],
+            'prefer-const': 'error',
+            'no-unused-vars': 'error',
+        },
+    },
 
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
 ];
