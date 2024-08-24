4b48dffed85ed9f51549aebbbf24df4a35a34795
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-24 17:28:44 +0300

    feat: lintting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['dist', 'node_modules']
+  },
+  { files: ['**/*.{ts}'] },
+  {
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prefer-const': 'error',
+      'no-unused-vars': 'error'
+    }
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier
+];
