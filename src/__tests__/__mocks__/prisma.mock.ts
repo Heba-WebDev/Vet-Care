a93c996b82cc8fa90b820df53ebdb61eacc65f7e
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-26 15:00:16 +0300

    feat: properly typed prisma mock

---
+++ src/__tests__/__mocks__/prisma.mock.ts
@@ -0,0 +1,65 @@
+import { PrismaClient } from "@prisma/client";
+import { vi } from "vitest";
+import { MockInstance } from "vitest";
+
+type MockPrismaClient = {
+  [K in keyof PrismaClient]: {
+    [M in keyof PrismaClient[K]]?: MockInstance;
+  };
+} & {
+  $transaction: MockInstance;
+};
+
+export const prismaMock: MockPrismaClient = {
+    staff: {
+        findFirst: vi.fn(),
+        findMany: vi.fn(),
+        create: vi.fn(),
+        update: vi.fn(),
+        delete: vi.fn(),
+    },
+    jobs: {
+        findFirst: vi.fn()
+    },
+    veterinarians: {
+        findFirst: vi.fn(),
+        findMany: vi.fn(),
+        create: vi.fn(),
+        update: vi.fn(),
+        delete: vi.fn(),
+    },
+    owners: {
+        findFirst: vi.fn(),
+        findMany: vi.fn(),
+        create: vi.fn(),
+        count: vi.fn(),
+        update: vi.fn(),
+    },
+    pets: {},
+    animals: {},
+    permissions: {},
+    appointments: {},
+    services: {},
+    transactions: {},
+    notifications: {},
+    publicHolidays: {},
+    formerStaff: {
+        create: vi.fn(),
+        findMany: vi.fn(),
+    },
+    formerVets: {
+        create: vi.fn(),
+        findMany: vi.fn(),
+    },
+    $on: vi.fn(),
+    $connect: vi.fn(),
+    $disconnect: vi.fn(),
+    $use: vi.fn(),
+    $executeRaw: vi.fn(),
+    $executeRawUnsafe: vi.fn(),
+    $queryRaw: vi.fn(),
+    $queryRawUnsafe: vi.fn(),
+    $transaction: vi.fn(),
+    $extends: {},
+};
+
