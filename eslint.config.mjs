ce5cd45b0998dad2d3351e5df95fef5606710572
Heba-Webdev23 <jtvonline@yahoo.com>
2024-08-26 19:41:49 +0300

    feat: prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,45 @@
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   {
-    ignores: ["dist", "node_modules"],
+    ignores: ['dist', 'node_modules'],
   },
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
+        prettier: prettierPlugin,
       },
       globals: {
         ...globals.node,
       },
     },
+    prettierConfig,
     plugins: {
-      "@typescript-eslint": tseslint.plugin,
+      '@typescript-eslint': tseslint.plugin,
     },
+
     rules: {
-      semi: ["error", "always"],
-      quotes: ["error", "single"],
-      "prefer-const": "error",
-      "no-unused-vars": "on",
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      eqeqeq: ["error", "always"],
-      curly: "error",
-      "no-var": "error",
+      'prettier/prettier': 'error',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prefer-const': 'error',
+      'no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      eqeqeq: ['error', 'always'],
+      curly: 'error',
+      'no-var': 'error',
     },
   },
   ...tseslint.configs.recommended,
-  ...tseslint.configs.recommendedRequiringTypeChecking,
   pluginJs.configs.recommended,
 ];
