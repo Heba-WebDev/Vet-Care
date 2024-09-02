dc2e8254628850077d1094a845bbe6de3909ebee
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-02 15:21:52 +0300

    feat: animal mapper

---
+++ src/features/Animals/infrastructure/mapper/animals.mapper.ts
@@ -0,0 +1,14 @@
+import { CustomError } from "../../../../domain";
+import { AnimalEntity } from "../../domain";
+
+export class AnimalMapper {
+    static animalEntityFromObject(animal: AnimalEntity) {
+        const { type, isSupported, isDeleted} = animal;
+
+        if (!type) throw CustomError.badRequest('Missing type');
+        if (typeof isSupported !== 'boolean') throw CustomError.badRequest('isSupported must be a boolean');
+        if (typeof isDeleted !== 'boolean') throw CustomError.badRequest('isDeleted must be a boolean');
+
+        return new AnimalEntity(type, isSupported, isDeleted);
+    }
+}
