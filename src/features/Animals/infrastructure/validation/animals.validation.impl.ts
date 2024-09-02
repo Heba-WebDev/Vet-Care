eb88233600ef039d54e2bb51e55e3b0fb1f0633e
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-02 15:21:24 +0300

    feat: add animal

---
+++ src/features/Animals/infrastructure/validation/animals.validation.impl.ts
@@ -0,0 +1,10 @@
+import { AddAnimalsDto } from "../../domain/dtos";
+import { addAnimalSchema } from "./joi-schemas";
+
+export class AnimalsInputValidation {
+    add(animalDto: AddAnimalsDto): string | null {
+        const { error } = addAnimalSchema.validate(animalDto);
+        if (error) return error.message;
+        return null;
+    }
+}
