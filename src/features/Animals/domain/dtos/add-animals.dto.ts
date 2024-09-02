378f5657683c0663722969a51720f7adc4d756ca
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-02 15:18:27 +0300

    feat: add animal dto

---
+++ src/features/Animals/domain/dtos/add-animals.dto.ts
@@ -0,0 +1,16 @@
+import { AnimalsInputValidation } from "../../infrastructure";
+
+export class AddAnimalsDto {
+    private constructor(
+        public readonly type: string,
+        public readonly isSupported: boolean = true
+    ) {}
+
+    static add(object: {[key: string]: string | boolean}): [string?, AddAnimalsDto?] {
+        const { type, isSupported } = object;
+        const animalDto = new AddAnimalsDto(type as string, isSupported as boolean);
+        const error = new AnimalsInputValidation().add(animalDto);
+        if (error) return [error, undefined];
+        return [undefined, animalDto];
+    }
+}
---
+++ src/features/Animals/domain/dtos/index.ts
@@ -0,0 +1,1 @@
+export * from './add-animals.dto';
