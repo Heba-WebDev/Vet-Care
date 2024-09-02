b5c813f70f4b050f0d4946180c690e1902054985
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-02 15:24:54 +0300

    fix: added next

--- src/presentation/middlewares/error.middleware.ts
+++ src/presentation/middlewares/error.middleware.ts
@@ -1,11 +1,15 @@
-import { Request, Response } from 'express';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response, NextFunction } from 'express';
+import { logger } from '../../infrastructure';
 
 export class GlobalErrorMiddleware {
-  error(error: Error, req: Request, res: Response) {
+  error(error: Error, req: Request, res: Response, next: NextFunction) {
+    logger.error(`Error occurred: ${error.message}`, { error });
     res.status(500).send({ error: error.message });
   }
 
-  notFound(req: Request, res: Response) {
+  notFound(req: Request, res: Response, next: NextFunction) {
     res.status(404).send({ error: 'Resource not found' });
   }
 }
