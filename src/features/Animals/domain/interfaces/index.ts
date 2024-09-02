8bc44f2d00b5e5aa828a7b0dc49f13cc44a46fad
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-02 15:19:44 +0300

    feat: animal add use-case interface

---
+++ src/features/Animals/domain/interfaces/index.ts
@@ -0,0 +1,14 @@
+import { AddAnimalsDto } from '../dtos';
+import { AnimalEntity } from '../entities';
+
+// types
+export type AnimalsStandardResponse = {
+  status: string;
+  message: string | null;
+  data: AnimalEntity | null;
+}
+
+// interfaces
+export interface AddAnimalUseCase {
+    execute(animalDto: AddAnimalsDto): Promise<AnimalsStandardResponse>;
+}
