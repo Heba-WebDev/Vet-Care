c3d1ab94c7f54de08aceab8c88dce4ea697167a8
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-05 21:47:32 +0300

    feat: delete animal schema

--- src/features/Animals/infrastructure/validation/animals.validation.impl.ts
+++ src/features/Animals/infrastructure/validation/animals.validation.impl.ts
@@ -1,9 +1,15 @@
-import { AddAnimalsDto } from '../../domain/dtos';
-import { addAnimalSchema } from './joi-schemas';
+import { AddAnimalsDto, DeleteAnimalDto } from '../../domain/dtos';
+import { addAnimalSchema, deleteAnimalSchema } from './joi-schemas';
 
 export class AnimalsInputValidation {
   add(animalDto: AddAnimalsDto): string | null {
     const { error } = addAnimalSchema.validate(animalDto);
+    if (error) return error.message;
+    return null;
+  }
+
+  delete(animalDto: DeleteAnimalDto): string | null {
+    const { error } = deleteAnimalSchema.validate(animalDto);
     if (error) return error.message;
     return null;
   }
