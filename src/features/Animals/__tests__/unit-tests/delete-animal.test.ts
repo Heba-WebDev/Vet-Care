bdb72f870eee9fbb655c7bf0bdab017166c7723b
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-08 15:11:29 +0300

    style: format

--- src/features/Animals/__tests__/unit-tests/delete-animal.test.ts
+++ src/features/Animals/__tests__/unit-tests/delete-animal.test.ts
@@ -8,34 +8,33 @@ import { CustomError } from '../../../../domain';
 import { describe } from 'node:test';
 
 describe('Delete an animal', () => {
-    let animalsDatasource: AnimalsDatasourceImpl;
+  let animalsDatasource: AnimalsDatasourceImpl;
 
-    beforeEach(() => {
-        animalsDatasource = new AnimalsDatasourceImpl(prismaMock as unknown as PrismaClient);
-        vi.clearAllMocks();
-    });
+  beforeEach(() => {
+    animalsDatasource = new AnimalsDatasourceImpl(prismaMock as unknown as PrismaClient);
+    vi.clearAllMocks();
+  });
 
-    it('should successfully soft delete an animal', async() => {
-        prismaMock.animals.findFirst?.mockReturnValueOnce(animalMock);
-        prismaMock.animals.update?.mockReturnValueOnce(animalMockDeleted);
-        AnimalMapper.animalEntityFromObject(animalMockDeleted);
-        const result = await animalsDatasource.delete({ id: 1});
-        expect(typeof result).toEqual('object');
-        expect(result?.isDeleted).toEqual(true);
-    });
+  it('should successfully soft delete an animal', async () => {
+    prismaMock.animals.findFirst?.mockReturnValueOnce(animalMock);
+    prismaMock.animals.update?.mockReturnValueOnce(animalMockDeleted);
+    AnimalMapper.animalEntityFromObject(animalMockDeleted);
+    const result = await animalsDatasource.delete({ id: 1 });
+    expect(typeof result).toEqual('object');
+    expect(result?.isDeleted).toEqual(true);
+  });
 
-    it('should throw an error if the animal is not found', async() => {
-        prismaMock.animals.findFirst?.mockReturnValueOnce(animalMockDeleted);
-        await expect(animalsDatasource.delete({ id: 1 })).rejects.toThrow(
-            CustomError.badRequest('Animal already deleted')
-        )
-    });
+  it('should throw an error if the animal is not found', async () => {
+    prismaMock.animals.findFirst?.mockReturnValueOnce(animalMockDeleted);
+    await expect(animalsDatasource.delete({ id: 1 })).rejects.toThrow(
+      CustomError.badRequest('Animal already deleted'),
+    );
+  });
 
-    it('should throw an error if animal already deleted', async() => {
-        prismaMock.animals.findFirst?.mockReturnValueOnce(null);
-        await expect(animalsDatasource.delete({ id: 1 })).rejects.toThrow(
-            CustomError.badRequest('No animal found')
-        )
-    });
-
+  it('should throw an error if animal already deleted', async () => {
+    prismaMock.animals.findFirst?.mockReturnValueOnce(null);
+    await expect(animalsDatasource.delete({ id: 1 })).rejects.toThrow(
+      CustomError.badRequest('No animal found'),
+    );
+  });
 });
