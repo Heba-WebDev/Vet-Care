b19a3371e151e37b92ba7324406fb7596b037eb4
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-08 15:15:27 +0300

    style: format

--- src/features/Animals/domain/dtos/delete-animal.dto.ts
+++ src/features/Animals/domain/dtos/delete-animal.dto.ts
@@ -1,14 +1,12 @@
 import { AnimalsInputValidation } from '../../infrastructure';
 
 export class DeleteAnimalDto {
-    private constructor(
-        public readonly id: number,
-    ) {}
+  private constructor(public readonly id: number) {}
 
-    static delete(id: string): [string?, DeleteAnimalDto?] {
-        const animalDto = new DeleteAnimalDto(parseInt(id as string));
-        const error = new AnimalsInputValidation().delete(animalDto);
-        if (error) return [error, undefined];
-        return [undefined, animalDto];
-    }
+  static delete(id: string): [string?, DeleteAnimalDto?] {
+    const animalDto = new DeleteAnimalDto(parseInt(id as string));
+    const error = new AnimalsInputValidation().delete(animalDto);
+    if (error) return [error, undefined];
+    return [undefined, animalDto];
+  }
 }
--- src/features/Animals/infrastructure/validation/joi-schemas/delete-animal.schema.ts
+++ src/features/Animals/infrastructure/validation/joi-schemas/delete-animal.schema.ts
@@ -1,8 +1,8 @@
 import joi from 'joi';
 
 export const deleteAnimalSchema = joi.object({
-    id: joi.number().required().messages({
-        "id.number": "A valid animal id is required",
-        "id.required": "An animal id is required"
-    })
+  id: joi.number().required().messages({
+    'id.number': 'A valid animal id is required',
+    'id.required': 'An animal id is required',
+  }),
 });
