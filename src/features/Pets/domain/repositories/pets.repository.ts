7a22194e4bed557d50bd82f8860c4db677308b96
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-10 18:34:25 +0300

    feat: get-all pets

--- src/features/Pets/domain/repositories/pets.repository.ts
+++ src/features/Pets/domain/repositories/pets.repository.ts
@@ -1,6 +1,7 @@
-import { RegisterPetDto } from '../dtos';
+import { RegisterPetDto, GetAllPetsDto } from '../dtos';
 import { PetEntity } from '../entities';
 
 export abstract class PetsRepository {
   abstract register(dto: RegisterPetDto): Promise<PetEntity | null>;
+  abstract getAll(dto: GetAllPetsDto): Promise<PetEntity[] | null>;
 }
--- src/features/Pets/infrastructure/repositories/pets.repository.impl.ts
+++ src/features/Pets/infrastructure/repositories/pets.repository.impl.ts
@@ -1,4 +1,4 @@
-import { PetsDatasource, PetsRepository, PetEntity, RegisterPetDto } from '../../domain';
+import { PetsDatasource, PetsRepository, PetEntity, RegisterPetDto, GetAllPetsDto } from '../../domain';
 
 export class PetsRepositoryImpl extends PetsRepository {
   constructor(private readonly datasource: PetsDatasource) {
@@ -7,5 +7,9 @@ export class PetsRepositoryImpl extends PetsRepository {
 
   register(dto: RegisterPetDto): Promise<PetEntity | null> {
     return this.datasource.register(dto);
+  }
+
+  getAll(dto: GetAllPetsDto): Promise<PetEntity[] | null> {
+    return this.datasource.getAll(dto);
   }
 }
