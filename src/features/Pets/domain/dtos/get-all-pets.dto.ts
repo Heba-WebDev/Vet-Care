b5c2f06c9c8f00ddfe958c9a0076a351b3290ba9
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-10 18:34:38 +0300

    feat: get-all pets dto

---
+++ src/features/Pets/domain/dtos/get-all-pets.dto.ts
@@ -0,0 +1,14 @@
+import { PetsInputValidation } from '../../infrastructure/validation';
+
+export class GetAllPetsDto {
+    private constructor(
+        public readonly owner_id: string,
+    ) {}
+
+    static getAll(owner_id: string): [string?, GetAllPetsDto?] {
+        const dto = new GetAllPetsDto(owner_id);
+        const error = new PetsInputValidation().getAll(dto);
+        if (error) return [error, undefined];
+        return [undefined, dto];
+    }
+}
