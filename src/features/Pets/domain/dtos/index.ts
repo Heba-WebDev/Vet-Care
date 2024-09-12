99b9f85cf7518cb46882d49221a7a86d26334e8e
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-12 19:12:15 +0300

    chore: updated exports

--- src/features/Pets/domain/dtos/index.ts
+++ src/features/Pets/domain/dtos/index.ts
@@ -1,2 +1,3 @@
 export * from './register-pet.dto';
 export * from './get-all-pets.dto';
+export * from './update-pet.dto';
--- src/features/Pets/domain/use-cases/index.ts
+++ src/features/Pets/domain/use-cases/index.ts
@@ -1,2 +1,3 @@
 export * from './register-pet.use-case';
 export * from './get-all-pets.use-case';
+export * from './update-pet.use-case';
