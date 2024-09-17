f19ffbd0b54e995e6bf6784ffd7ea4054bfb15f9
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-17 18:35:35 +0300

    feat: add service route

---
+++ src/features/Services/presentation/routes.services.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { AuthMiddleware } from '../../../presentation';
+import { ServicesController } from './controller.services';
+import { ServicesDatasourceImpl, ServicesRepositoryImpl } from '../infrastructure';
+
+export class ServicesRoutes {
+  static get routes(): Router {
+    const router = Router();
+    const datasource = new ServicesDatasourceImpl();
+    const repository = new ServicesRepositoryImpl(datasource);
+    const controller = new ServicesController(repository);
+    router.post('/', [AuthMiddleware.authenticated], controller.addAService);
+    return router;
+  }
+}
