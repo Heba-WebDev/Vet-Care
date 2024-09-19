c33a09fbc4eda5ceae1ce2815696227a0da171ee
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-19 19:18:23 +0300

    feat: activate service dto

--- src/features/Services/domain/dtos/activatet-service.dto.ts
+++ src/features/Services/domain/dtos/activatet-service.dto.ts
@@ -1,3 +1,5 @@
+import { ServicesInputValidation } from '../../infrastructure/validation/services.validation.impl';
+
 export class ActivateServiceDto {
   private constructor(public id: number) {}
 
@@ -5,6 +7,8 @@ export class ActivateServiceDto {
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) return ['Invalid id', undefined];
     const dto = new ActivateServiceDto(parsedId);
+    const error = new ServicesInputValidation().activate(dto);
+    if (error) return [error, undefined];
     return [undefined, dto];
   }
 }
