7c46937481de8ae68853bb26079360d6ea3cb27f
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-23 19:29:46 +0300

    feat: update working day

---
+++ src/features/Working-Days/domain/repositories/working-days.repository.ts
@@ -0,0 +1,6 @@
+import { UpdateWorkingDayDto } from '../dtos';
+import { WorkingDayEntity } from '../entities';
+
+export abstract class WorkingDaysRepository {
+  abstract update(dto: UpdateWorkingDayDto): Promise<WorkingDayEntity | null>;
+}
