f5d2372867509fd97dc3e043b8a6e6056ca227c1
Heba-Webdev23 <jtvonline@yahoo.com>
2024-09-23 19:31:36 +0300

    feat: update working day logic

---
+++ src/features/Working-Days/infrastructure/datasources/working-days.datasource.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+import { UpdateWorkingDayDto, WorkingDayEntity, WorkingDaysDatasource } from '../../domain';
+import { prisma } from '../../../../data';
+import { logger } from '../../../../infrastructure';
+import { CustomError } from '../../../../domain';
+import { WorkingDayMapper } from '../mappers';
+
+export class WorkingDaysDatasourceImpl implements WorkingDaysDatasource {
+  private readonly _prisma: PrismaClient;
+  constructor(orm: PrismaClient = prisma) {
+    this._prisma = orm;
+  }
+
+  async update(dto: UpdateWorkingDayDto): Promise<WorkingDayEntity | null> {
+    const { id, active } = dto;
+    try {
+      const workingDay = await this._prisma.$transaction(async (prisma) => {
+        const exists = await prisma.workingDays.findFirst({ where: { id } });
+        if (!exists) throw CustomError.badRequest('No working day found');
+        return prisma.workingDays.update({
+          where: {
+            id,
+          },
+          data: {
+            active,
+          },
+        });
+      });
+      return WorkingDayMapper.workingDayEntityFromObject(workingDay);
+    } catch (error) {
+      logger.error(error);
+      if (error instanceof CustomError) throw error;
+      throw CustomError.internalServerError();
+    }
+  }
+}
