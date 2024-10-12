eb9e3313fe038dab663a7d0d3e0344acc720d045
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-12 23:14:37 +0300

    feat: add holiday route

---
+++ src/features/Holidays/preseentation/routes.holidays.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { HolidaysDatasourceImpl, HolidaysRepositoryIml } from '../infrastructure';
+import { HolidaysController } from './controller.holidays';
+import { AuthMiddleware } from '../../../presentation';
+
+export class HolidaysRoutes {
+  static get routes(): Router {
+    const router = Router();
+    const datasource = new HolidaysDatasourceImpl();
+    const repository = new HolidaysRepositoryIml(datasource);
+    const controller = new HolidaysController(repository);
+
+    router.post('/', [AuthMiddleware.authenticated, AuthMiddleware.authorized], controller.add);
+    return router;
+  }
+}
