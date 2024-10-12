9394fb22ea9061f511ec684730eba65664072749
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-13 00:11:03 +0300

    feat: holidays mock

--- src/features/Holidays/__tests__/mocks/index.ts
+++ src/features/Holidays/__tests__/mocks/index.ts
@@ -1,6 +1,21 @@
-import { AddHolidayDto } from '../../domain';
+import { AddHolidayDto, HolidayEntity } from '../../domain';
 
 export const addHolidayDtoMock: AddHolidayDto = {
   name: 'New Year',
   date: '01/01/2025',
 };
+
+export const holidaysMock: HolidayEntity[] = [
+  {
+    name: 'New Year',
+    date: '01/01/2025'
+  },
+  {
+    name: 'Easter',
+    date: '20/04/2025'
+  },
+  {
+    name: 'Labor Day',
+    date: '01/09/2025'
+  },
+]
