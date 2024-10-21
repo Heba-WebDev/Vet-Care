ea6351aacba91ef00f5f3597d68e29a19ce235d1
Heba-Webdev23 <jtvonline@yahoo.com>
2024-10-22 01:10:35 +0300

    fix: code format

--- src/features/Holidays/__tests__/mocks/index.ts
+++ src/features/Holidays/__tests__/mocks/index.ts
@@ -8,14 +8,14 @@ export const addHolidayDtoMock: AddHolidayDto = {
 export const holidaysMock: HolidayEntity[] = [
   {
     name: 'New Year',
-    date: '01/01/2025'
+    date: '01/01/2025',
   },
   {
     name: 'Easter',
-    date: '20/04/2025'
+    date: '20/04/2025',
   },
   {
     name: 'Labor Day',
-    date: '01/09/2025'
+    date: '01/09/2025',
   },
-]
+];
