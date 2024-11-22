8fd9345b213ce35d2997bc2edeffea8741a8f5f7
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-22 18:54:54 +0300

    feat: previous holidays mock

--- src/features/Holidays/__tests__/mocks/index.ts
+++ src/features/Holidays/__tests__/mocks/index.ts
@@ -2,35 +2,53 @@ import { AddHolidayDto, UpdateHolidayDto, HolidayEntity } from '../../domain';
 
 export const addHolidayDtoMock: AddHolidayDto = {
   name: 'New Year',
-  date: '01/01/2025',
+  date: new Date(2025, 0, 1),
 };
 
 export const holidaysMock: HolidayEntity[] = [
   {
     id: '1',
     name: 'New Year',
-    date: '01/01/2025',
+    date: new Date(2025, 0, 1),
   },
   {
     id: '2',
     name: 'Easter',
-    date: '20/04/2025',
+    date: new Date(2025, 3, 20),
   },
   {
     id: '3',
     name: 'Labor Day',
-    date: '01/09/2025',
+    date: new Date(2025, 8, 1),
+  },
+];
+
+export const previousHolidaysMock: HolidayEntity[] = [
+  {
+    id: '1',
+    name: 'New Year',
+    date: new Date(2024, 0, 1),
+  },
+  {
+    id: '2',
+    name: 'Easter',
+    date: new Date(2024, 3, 20),
+  },
+  {
+    id: '3',
+    name: 'Labor Day',
+    date: new Date(2024, 8, 1),
   },
 ];
 
 export const updateHolidayDtoMock: UpdateHolidayDto = {
   id: '1',
   name: 'New Year',
-  date: '01/01/2025',
+  date: new Date(2025, 0, 1),
 };
 
 export const updatedHolidayMock: UpdateHolidayDto = {
   id: '1',
   name: 'Labor Day',
-  date: '01/02/2025',
+  date: new Date(2025, 1, 1),
 };
