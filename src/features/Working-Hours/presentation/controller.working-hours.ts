a1e0b6e944b3b6c6be52029172577aad6eb1b6c0
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-29 19:22:52 +0300

    feat: get-working-hours controller

--- src/features/Working-Hours/presentation/controller.working-hours.ts
+++ src/features/Working-Hours/presentation/controller.working-hours.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 import { BaseController } from '../../../presentation/base.controller';
-import { AddWorkingHours, AddWorkingHoursDto, WorkingHoursRepository } from '../domain';
+import {
+  AddWorkingHours,
+  AddWorkingHoursDto,
+  GetWorkingHoursDto,
+  WorkingHoursRepository,
+} from '../domain';
+import { GetWorkingHours } from '../domain/use-cases/get-working-hours.use-case';
 
 export class WorkingHoursController extends BaseController {
   constructor(private readonly repo: WorkingHoursRepository) {
@@ -10,6 +16,15 @@ export class WorkingHoursController extends BaseController {
     const [error, dto] = AddWorkingHoursDto.add(req.params.vet_id, req.body);
     if (error) return res.status(400).send({ error });
     new AddWorkingHours(this.repo)
+      .execute(dto!)
+      .then((data) => res.send(data))
+      .catch((error) => this.handleError(error, res));
+  };
+
+  get = (req: Request, res: Response) => {
+    const [error, dto] = GetWorkingHoursDto.getWorkingHours(req.params.vet_id, req.query);
+    if (error) return res.status(400).send({ error });
+    new GetWorkingHours(this.repo)
       .execute(dto!)
       .then((data) => res.send(data))
       .catch((error) => this.handleError(error, res));
