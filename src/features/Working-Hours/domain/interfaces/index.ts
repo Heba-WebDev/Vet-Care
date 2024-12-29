5d9055e1e24596529a0df6272373e972f86af98d
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-29 19:21:09 +0300

    feat: get-working-hours use case interface

--- src/features/Working-Hours/domain/interfaces/index.ts
+++ src/features/Working-Hours/domain/interfaces/index.ts
@@ -1,14 +1,18 @@
-import { AddWorkingHoursDto } from '../dtos';
+import { AddWorkingHoursDto, GetWorkingHoursDto } from '../dtos';
 import { WorkingHoursEntity } from '../entities';
 
 // types
 export type WorkingHoursStandardRespons = {
   status: string;
   message: string | null;
-  data: WorkingHoursEntity | null;
+  data: WorkingHoursEntity | WorkingHoursEntity[] | null;
 };
 
 // interfaces
 export interface AddWorkingHoursUseCase {
   execute(dto: AddWorkingHoursDto): Promise<WorkingHoursStandardRespons>;
+}
+
+export interface GetWorkingHoursUseCase {
+  execute(dto: GetWorkingHoursDto): Promise<WorkingHoursStandardRespons>;
 }
